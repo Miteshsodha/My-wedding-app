3618f27dc21f1c7d28e1f7454e406d29fbb24825
Mitesh Sodhaparmar <[sodhamitesh66@gmail.com]>
2026-04-19 15:58:19 +0530

    Vire Di Wedding Hai

---
+++ src/constants/wedding.ts
@@ -0,0 +1,25 @@
+export const WEDDING_CONFIG = {
+  date: new Date('2026-04-26T10:00:00'),
+  bride: {
+    name: 'Daxa',
+    fullName: 'Daxa Dabhi',
+    photo: '/assets/images/bride-circle.png',
+  },
+  groom: {
+    name: 'Jaydip',
+    fullName: 'Jaydip Sodhaparmar',
+    photo: '/assets/images/groom-circle.png',
+  },
+  venue: {
+    ceremony: {
+      name: 'Maholel',
+      address: 'Maholel , Nadiad , Gujarat',
+      time: '10:00 AM',
+    },
+    reception: {
+      name: 'Bhaidva',
+      address: 'Masra , Kheda , Gujarat',
+      time: '25/4/2026 10:00 AM',
+    },
+  },
+};
