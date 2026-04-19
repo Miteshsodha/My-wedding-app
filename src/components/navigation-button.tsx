7d112131cbb329cb8847553751ab735fea292e96
Mitesh Sodhaparmar <169137816+Miteshsodha@users.noreply.github.com>
2026-04-19 16:55:50 +0530

    Vire Di Wedding Hai

--- src/components/navigation-button.tsx
+++ src/components/navigation-button.tsx
@@ -93,7 +93,6 @@ export function NavigationButton({
             initial={NAVIGATION_ANIMATIONS.background.initial}
             animate={{ scale: 1, opacity: 1 }}
             exit={NAVIGATION_ANIMATIONS.background.exit}
-          
             className="absolute -bottom-0.5 sm:-bottom-1 left-1/2 -translate-x-1/2 w-0.5 sm:w-1 h-0.5 sm:h-1 bg-white rounded-full shadow-lg z-10"
           >
             <motion.div
